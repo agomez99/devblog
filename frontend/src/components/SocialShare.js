5454c5f264f4a360315878f4070f6f4e09656b8f
Austine Gomez <austine.gomez@yahoo.com>
2021-01-30 19:13:52 -0600

    Update SocialShare.js

--- frontend/src/components/SocialShare.js
+++ frontend/src/components/SocialShare.js
@@ -2,8 +2,15 @@ import React from "react";
   import { Twitter, Facebook, Linkedin, } from 'react-social-sharing'
 
   export default function SocialShare() {
+
+    const styleObj = {
+      fontSize: 14,
+      color: "#4a54f1",
+      textAlign: "center",
+      paddingTop: "100px",
+  }
     return (
-        <div class="social-share">
+        <div class="social-share" style={styleObj}>
               <Twitter link={"https://agblog.gtsb.io/" } />   
               <Facebook  link={"https://agblog.gtsb.io/" } /> 
               <Linkedin  link={"https://agblog.gtsb.io/" } />              
